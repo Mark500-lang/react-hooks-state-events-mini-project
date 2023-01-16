42e4d1b5c89d4710ebb8e6312c3418f7b584f05e
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-01-16 04:06:30 +0300

    completed assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { TASKS } from "../data";
 
-function TaskList() {
+function TaskList({tasks,deletedItem}) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map((task,index)=>(
+        <TASKS 
+          deletedItem={deletedItem}
+          key={index}
+          text={task.text}
+          category={task.category}
+         />
+      ))}
     </div>
   );
 }
